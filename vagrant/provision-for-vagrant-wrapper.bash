369fcce59684836a6dbfb05935fa64dc861e2f5d
Gene Wood <gene_wood@cementhorizon.com>
2012-10-25 17:13:36 -0700

    Adding bit.ly links

--- vagrant/provision-for-vagrant-wrapper.bash
+++ vagrant/provision-for-vagrant-wrapper.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-# To install run curl -L http://bit.ly/notsureyet | bash
+# To install run curl -L http://bit.ly/TZLYrX | bash
 #
 curl -L https://raw.github.com/gene1wood/tools/master/vagrant/provision-for-vagrant.bash | bash | 2>&1 | tee -a /tmp/provision-for-vagrant.log
 echo "Read the provisioning log here : /tmp/provision-for-vagrant.log"
