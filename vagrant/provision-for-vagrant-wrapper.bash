5c57c9362326363208f0385fb92248412946daef
Gene Wood <gene_wood@cementhorizon.com>
2012-10-25 17:27:42 -0700

    fixed comment

--- vagrant/provision-for-vagrant-wrapper.bash
+++ vagrant/provision-for-vagrant-wrapper.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 # To install run curl -L http://bit.ly/TZLYrX | bash
 #
-curl -L https://raw.github.com/gene1wood/tools/master/vagrant/provision-for-vagrant.bash | bash | 2>&1 | tee -a /tmp/provision-for-vagrant.log
+curl -L https://raw.github.com/gene1wood/tools/master/vagrant/provision-for-vagrant.bash | bash 2>&1 | tee -a /tmp/provision-for-vagrant.log
 echo "Read the provisioning log here : /tmp/provision-for-vagrant.log"
 
